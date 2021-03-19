ab0d86e3743e68b5cc89e9f5e92f01febbc9e408
Shvetank Prakash <sprakash@Shvetanks-MacBook-Pro.local>
2021-03-18 19:24:56 -0700

    Went through flow from start to end.

--- 4115_project/yagl.ml
+++ 4115_project/yagl.ml
@@ -5,21 +5,19 @@ Top-level of the YAGL compiler: scan & parse the input,
    check the resulting AST and generate an SAST from it, generate LLVM IR,
    and dump the module *)
 
-type action = Ast | Compile (* | Sast | LLVM_IR *)
+type action = Ast | Compile  | Sast  | LLVM_IR 
 
 let () =
   let action = ref Compile in
   let set_action a () = action := a in
   let speclist = [
     ("-a", Arg.Unit (set_action Ast), "Print the AST");
-    (*
     ("-s", Arg.Unit (set_action Sast), "Print the SAST");
     ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
-    *)
     ("-c", Arg.Unit (set_action Compile),
       "Check and print the generated LLVM IR (default)");
   ] in  
-  let usage_msg = "usage: ./yagl.native [-a|-s|-l|-c] [file.mc]" in
+  let usage_msg = "usage: ./yagl.native [-a|-s|-l|-c] [file.ygl]" in
   let channel = ref stdin in
   Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
   
@@ -27,9 +25,7 @@ let () =
   let ast = Yaglparse.program Scanner.token lexbuf in  
   match !action with
     Ast -> print_string (Ast.string_of_program ast)
-    | Compile -> ()
-  (*
-  | _ -> let sast = Semant.check ast in
+    | _ -> let sast = Semant.check ast in
     match !action with
       Ast     -> ()
     | Sast    -> print_string (Sast.string_of_sprogram sast)
@@ -37,4 +33,4 @@ let () =
     | Compile -> let m = Codegen.translate sast in
 	Llvm_analysis.assert_valid_module m;
 	print_string (Llvm.string_of_llmodule m)
-  *)
+  
