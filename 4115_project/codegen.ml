cd55595bca8987226da9660983e9065ed3d80f37
James Mastran <Jam2454@columbia.edu>
2021-04-03 23:56:07 -0400

    Added binop base

--- 4115_project/codegen.ml
+++ 4115_project/codegen.ml
@@ -108,6 +108,34 @@ let translate functions =
     let rec expr builder ((_, e) : sexpr) = match e with
 	      SLiteral i  -> L.const_int i32_t i
       | SId s       -> L.build_load (lookup s) s builder
+      (*| SBinop ((A.Float,_ ) as e1, op, e2) ->
+	  let e1' = expr builder e1
+	  and e2' = expr builder e2 in
+	  (match op with 
+	    A.Add     -> L.build_fadd
+	  | A.Sub     -> L.build_fsub
+	  | A.Mult    -> L.build_fmul
+	  | A.Div     -> L.build_fdiv 
+	  | A.Equal   -> L.build_fcmp L.Fcmp.Oeq
+	  | A.Less    -> L.build_fcmp L.Fcmp.Olt
+	  | A.Greater -> L.build_fcmp L.Fcmp.Ogt
+	  | A.And | A.Or ->
+	      raise (Failure "internal error: semant should have rejected and/or on float")
+	  ) e1' e2' "tmp" builder*)
+      | SBinop (e1, op, e2) ->
+	  let e1' = expr builder e1
+	  and e2' = expr builder e2 in
+	  (match op with
+	    A.Add     -> L.build_add
+	  | A.Sub     -> L.build_sub
+	  | A.Mult    -> L.build_mul
+          | A.Div     -> L.build_sdiv
+	  | A.And     -> L.build_and
+	  | A.Or      -> L.build_or
+	  | A.Equal   -> L.build_icmp L.Icmp.Eq
+	  | A.Less    -> L.build_icmp L.Icmp.Slt
+	  | A.Greater -> L.build_icmp L.Icmp.Sgt
+	  ) e1' e2' "tmp" builder
       | SStrLit  s  -> L.build_global_stringptr s "fmt" builder
       | SCall ("printInt", [e]) | SCall ("printb", [e]) ->
 	  L.build_call printf_func [| int_format_str ; (expr builder e) |]
--- 4115_project/semant.ml
+++ 4115_project/semant.ml
@@ -151,9 +151,26 @@ let check_function func =
        | Literal  l -> (Int, SLiteral l)
        | StrLit s -> (String, SStrLit s)
        | Id s       -> (type_of_identifier s, SId s)
-
+       | Binop(e1, op, e2) as e -> 
+          let (t1, e1') = expr e1 
+          and (t2, e2') = expr e2 in
+          (* All binary operators require operands of the same type *)
+          let same = t1 = t2 in
+          (* Determine expression type based on operator and operand types *)
+          let ty = match op with
+            Add | Sub | Mult | Div when same && t1 = Int   -> Int
+          (*| Add when same && t1 = String   -> String*)
+          (*| Add | Sub | Mult | Div when same && t1 = Float -> Float
+          | Equal                  when same               -> Bool
+          | Less | Greater
+                     when same && (t1 = Int || t1 = Float) -> Bool
+          | And | Or when same && t1 = Bool -> Bool*)
+          | _ -> raise (
+	      Failure ("illegal binary operator " ^
+                       string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                       string_of_typ t2 ^ " in " ^ string_of_expr e))
+          in (ty, SBinop((t1, e1'), op, (t2, e2')))
       | BoolLit a -> raise (Failure("Boolit ERROR")) 
-      | Binop (a, b, c) -> raise (Failure("Binop ERROR")) 
       | Unop (a, b) -> raise (Failure("Unop ERROR")) 
       | Assign (a, b) -> raise (Failure("Assign ERROR")) 
       | Call (fname, args) as call -> raise (Failure("Call ERROR")) 
